3c687fba72436e2a56613321c183818c0a0eb50c
emanoelcampos <emanoel.cv@gmail.com>
2023-08-01 18:26:58 -0300

    alterando registros da tabela de produtos usando o comando UPDATE

---
+++ src/dev/emanoel/modulo04/alterando-registros-produtos.sql
@@ -0,0 +1,12 @@
+UPDATE tbProduto 
+	SET
+		EMBALAGEM = 'Lata',
+		PRECO_LISTA = 2.46
+	WHERE
+		PRODUTO = '544931';
+
+UPDATE tbProduto 
+	SET
+		EMBALAGEM = 'Garrafa'
+	WHERE
+		PRODUTO = '1078680';
