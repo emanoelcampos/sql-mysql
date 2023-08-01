e001c89b3d8b82eea5a2cc1aa4dc0e7c834ae353
emanoelcampos <emanoel.cv@gmail.com>
2023-08-01 17:33:40 -0300

    criando primeiro registro na tabela de produtos e selecionando essa tabela para listar esse registro

---
+++ src/dev/emanoel/modulo03/primeiro-registro-tabela-produto.sql
@@ -0,0 +1,14 @@
+INSERT INTO tbProduto(
+	PRODUTO,
+	NOME,
+	EMBALAGEM,
+	TAMANHO,
+	SABOR,
+	PRECO_LISTA) 
+VALUES (
+	'1040107',
+	'Light - 350 ml - Melância',
+	'Lata',
+	'350 ml',
+	'Melância',
+	4.56);
