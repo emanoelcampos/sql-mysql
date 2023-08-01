73157a95ee66f6b3a7fb8b76585f5ab8fdb4fc7e
emanoelcampos <emanoel.cv@gmail.com>
2023-08-01 17:57:32 -0300

    inserindo mais dados na tabela de produtos e na tabela de vendedores

---
+++ src/dev/emanoel/modulo04/inserindo-registros-tabela-vendedores.sql
@@ -0,0 +1,18 @@
+INSERT INTO tbVendedores(
+	MATRICULA,
+	NOME,
+	PERCENTUAL_COMISSAO)
+VALUES(
+	'00235',
+	'Márcio Almeida Silva',
+	0.08)
+
+INSERT INTO tbVendedores(
+	MATRICULA,
+	NOME,
+	PERCENTUAL_COMISSAO)
+VALUES(
+	'00236',
+	'Cláudia Morais',
+	0.08)
+
