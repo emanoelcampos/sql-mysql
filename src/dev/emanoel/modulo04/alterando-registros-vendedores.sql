2478f1bad59245edad08fffcd19bd15ccd1cbfc3
emanoelcampos <emanoel.cv@gmail.com>
2023-08-01 18:47:47 -0300

    alterando registros da tabela de vendedores usando o comando UPDATE

---
+++ src/dev/emanoel/modulo04/alterando-registros-vendedores.sql
@@ -0,0 +1,11 @@
+UPDATE tbVendedores
+	SET
+		PERCENTUAL_COMISSAO = 0.11
+	WHERE 
+		MATRICULA = '00236';
+
+UPDATE tbVendedores
+	SET
+		NOME = 'José Geraldo da Fonseca Junior'
+	WHERE 
+		MATRICULA = '00233';
