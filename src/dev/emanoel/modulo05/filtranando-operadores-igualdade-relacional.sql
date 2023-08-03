f8a4e9386a6fda7a9efcbc3657b1f76518106319
emanoelcampos <emanoel.cv@gmail.com>
2023-08-02 22:28:27 -0300

    filtrando dados usando o comando WHERE com operadores relacionais e de igualdade

---
+++ src/dev/emanoel/modulo05/filtranando-operadores-igualdade-relacional.sql
@@ -0,0 +1,48 @@
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	IDADE = 22;
+
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	IDADE > 22;
+
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	IDADE < 22;
+
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	IDADE <= 22;
+
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	IDADE <> 22;
+
+SELECT
+	*
+FROM
+	tbCliente
+WHERE
+	NOME > 'Fernanado Cavalcante';
+
+SELECT
+	*
+FROM
+	tbProduto
+WHERE
+	PRECO_LISTA = 16.008;
